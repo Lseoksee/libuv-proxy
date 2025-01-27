e8369639a3e45693b3822653ab8c935eb47bbfe0
Lseoksee <da864268@naver.com>
2025-01-27 17:50:29 +0900

    lib uv 추가

--- src/Main.c
+++ src/Main.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
-#include <winsock2.h>
-int main(int argc, char const *argv[]) {
-    WSADATA wsa;
-    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
-        printf("Failed. Error Code : %d", WSAGetLastError());
-        return 1;
-    }
+#include <uv.h>
 
-    SOCKET server = socket(AF_INET, SOCK_STREAM, 0);
-    if (server == INVALID_SOCKET) {
-        printf("Could not create socket : %d", WSAGetLastError());
-    }    
-    return 0;
+void main(int argc, char const *argv[]) {
+    printf("Hello World\n");
 }
